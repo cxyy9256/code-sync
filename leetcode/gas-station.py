de98337bf1a92cfdcd864f256691dfc0f55da672
cxyy9256 <cxyy9256@gmail.com>
2025-07-07 09:05:43 -0400

    Update solution for gas-station

--- leetcode/gas-station.py
+++ leetcode/gas-station.py
@@ -5,3 +5,14 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
+        if sum(gas) < sum(cost):
+            return -1
+        
+        current_gas = 0
+        start = 0 
+        for i in range(len(gas)):
+            current_gas += gas[i] - cost[i]
+            if current_gas < 0:
+                current_gas = 0
+                start = i+1 # keep going until you find positive one, guranteed sol.
+        return start
