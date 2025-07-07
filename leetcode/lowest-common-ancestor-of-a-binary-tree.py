38f52ef563019096dbe20b7595b981c258731a00
cxyy9256 <cxyy9256@gmail.com>
2025-07-07 08:38:56 -0400

    Add solution for lowest-common-ancestor-of-a-binary-tree

---
+++ leetcode/lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        
+        if not root:
+            return None
+        
+        if root == p or root == q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q) # none, p, q , or lowest ancestor on left 
+        
+        if left and right:
+            return root # the two nodes are on opposite sides of the root so then return root
+        
+        if left:
+            return left
+        
+        if right:
+            return right
