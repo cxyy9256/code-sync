5f22255be608b9e29ee2e3b36f2048e764aa674d
cxyy9256 <cxyy9256@gmail.com>
2025-07-07 09:26:45 -0400

    Update solution for min-stack

--- leetcode/min-stack.py
+++ leetcode/min-stack.py
@@ -1,31 +1,26 @@
 class MinStack(object):
 
     def __init__(self):
+        self.stack = []
+        self.min_stack = []
         
-
     def push(self, val):
-        """
-        :type val: int
-        :rtype: None
-        """
+        self.stack.append(val)
+        # min stack is empty
+        if not self.min_stack:
+            self.min_stack.append(val)
+        else:
+            self.min_stack.append(min(val, self.min_stack[-1]))
         
-
     def pop(self):
-        """
-        :rtype: None
-        """
+        self.stack.pop()
+        self.min_stack.pop()
         
-
     def top(self):
-        """
-        :rtype: int
-        """
+        return self.stack[-1]
         
-
     def getMin(self):
-        """
-        :rtype: int
-        """
+        return self.min_stack[-1]
         
 
 
