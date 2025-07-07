57374337bbf912ec347747eca9afb4f77816a215
cxyy9256 <cxyy9256@gmail.com>
2025-07-07 09:07:06 -0400

    Update solution for h-index

--- leetcode/h-index.py
+++ leetcode/h-index.py
@@ -4,3 +4,11 @@ class Solution(object):
         :type citations: List[int]
         :rtype: int
         """
+        citations.sort(reverse=True)
+        h_idx = 0
+        for i in range(len(citations)):
+            if citations[i] >= i+1:
+                h_idx = i+1
+            else:
+                break
+        return h_idx
