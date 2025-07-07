d01d7985f414caf10df51404d0426e1beffb5dee
cxyy9256 <cxyy9256@gmail.com>
2025-07-07 09:08:18 -0400

    Update solution for best-time-to-buy-and-sell-stock-ii

--- leetcode/best-time-to-buy-and-sell-stock-ii.py
+++ leetcode/best-time-to-buy-and-sell-stock-ii.py
@@ -4,3 +4,10 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        
+        # If you sell immediately at a higher price and then buy again later at a lower price, the sum of individual profits will always equal or exceed the profit you would get by waiting for a higher peak
+        max_profit = 0
+        for i in range(len(prices)-1):
+            if prices[i+1] > prices[i]:
+                max_profit += prices[i+1] - prices[i]
+        return max_profit
