0e7ac5eec22e0c44f18f833a9523a73314d23d98
cxyy9256 <cxyy9256@gmail.com>
2025-07-07 09:46:19 -0400

    Add solution for sum-root-to-leaf-numbers

---
+++ leetcode/sum-root-to-leaf-numbers.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def sumNumbers(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        
+        if not root:
+            return 0 
+        stack = [(root, 0)]
+        total_sum = 0
+        while stack:
+            node, current_sum = stack.pop()
+            if node:
+                current_sum = current_sum * 10 + node.val
+                if not node.left and not node.right:
+                    total_sum += current_sum
+                if node.right:
+                    stack.append((node.right, current_sum))
+                if node.left:
+                    stack.append((node.left, current_sum))
+        return total_sum
