e484798df4f1731dccffbfcd2d19abe785411082
cxyy9256 <cxyy9256@gmail.com>
2025-07-07 09:10:33 -0400

    Update solution for remove-duplicates-from-sorted-array

--- leetcode/remove-duplicates-from-sorted-array.py
+++ leetcode/remove-duplicates-from-sorted-array.py
@@ -4,3 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # first number always unique
+        j = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[j] = nums[i]
+                j += 1
+        return j
