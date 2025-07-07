e203ada5ad7e2c49187c7b4ca99529b6607518e3
cxyy9256 <cxyy9256@gmail.com>
2025-07-07 09:25:54 -0400

    Add solution for simplify-path

---
+++ leetcode/simplify-path.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+
+        stack = []
+        components = path.split("/")
+        for component in components:
+            if component == "" or component == ".":
+                continue
+            elif component == "..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(component)
+        return "/" + "/".join(stack)
