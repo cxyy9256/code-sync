dffde3810b468439c3d527d1d39114f6109e4ae0
cxyy9256 <cxyy9256@gmail.com>
2025-07-07 09:07:25 -0400

    Update solution for jump-game-ii

--- leetcode/jump-game-ii.py
+++ leetcode/jump-game-ii.py
@@ -4,3 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        
+        jumps = 0 
+        farthest = 0
+        current_end = 0 
+
+        for i in range(len(nums) -1): # edge case of [1] is just 0 jumps
+            farthest = max(farthest, i + nums[i])
+            if i == current_end:
+                jumps += 1
+                current_end = farthest
+            if current_end >= len(nums)-1:
+                break
+        return jumps
