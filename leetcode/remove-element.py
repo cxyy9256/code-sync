5d40ecc9cf6e38db90bb705eb89ff2b5ceede0bd
cxyy9256 <cxyy9256@gmail.com>
2025-07-07 09:11:00 -0400

    Update solution for remove-element

--- leetcode/remove-element.py
+++ leetcode/remove-element.py
@@ -5,3 +5,26 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
+        k = 0
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        
+        return k
+
+        '''
+        p_front = 0
+        p_back = len(nums)-1
+        while (p_back > p_front):
+            if (nums[p_back] == val):
+                p_back -= 1
+            if (nums[p_front] == val):
+                nums[p_front] = nums[p_back]
+                nums[p_back] = val
+            else:
+                p_front += 1
+        while (len(nums) > 0 and nums[-1] == val):
+            nums.pop(-1)
+        '''
