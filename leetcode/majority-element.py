a215e061d1015ddbf5114ffc28c87bf97ce3cea6
cxyy9256 <cxyy9256@gmail.com>
2025-07-07 09:09:38 -0400

    Update solution for majority-element

--- leetcode/majority-element.py
+++ leetcode/majority-element.py
@@ -4,3 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        candidate = None
+        count = 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count += 1
+            else: 
+                count -= 1 
+        
+        return candidate
