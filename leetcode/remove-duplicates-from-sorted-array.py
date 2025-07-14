ec41acd35baeb874353784b94dd4d2333adcde26
cxyy9256 <cxyy9256@gmail.com>
2025-07-13 21:07:25 -0400

    Update solution for remove-duplicates-from-sorted-array

--- leetcode/remove-duplicates-from-sorted-array.py
+++ leetcode/remove-duplicates-from-sorted-array.py
@@ -8,6 +8,7 @@ class Solution(object):
         j = 1
         for i in range(1, len(nums)):
             if nums[i] != nums[i - 1]:
+                # Whenever a new unique value is found at nums[i], it is copied forward into position j.
                 nums[j] = nums[i]
                 j += 1
         return j
