55a4f5c8baa611c7100268dc56b74b62076dd3fa
cxyy9256 <cxyy9256@gmail.com>
2025-07-13 23:03:44 -0400

    Add solution for move-zeroes

---
+++ leetcode/move-zeroes.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        # two pointer strategies
+        insert_pos = 0
+
+        for num in nums:
+            if num != 0:
+                nums[insert_pos] = num
+                insert_pos += 1
+            
+        while insert_pos < len(nums):
+            nums[insert_pos] = 0
+            insert_pos += 1
