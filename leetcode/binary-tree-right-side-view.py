d11128304c59a7f3ac1d11caadf75c4d768974ef
cxyy9256 <cxyy9256@gmail.com>
2025-07-15 16:29:12 -0400

    Update solution for binary-tree-right-side-view

--- leetcode/binary-tree-right-side-view.py
+++ leetcode/binary-tree-right-side-view.py
@@ -10,6 +10,9 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: List[int]
         """
+        '''
+        A deque (pronounced "deck") is a double-ended queue. While a standard queue is a linear data structure following the FIFO (First-In, First-Out) principle, meaning elements are added at one end (rear) and removed from the other (front), a deque offers greater flexibility. It allows elements to be inserted and deleted from both the front and the rear. 
+        '''
         if not root:
             return []
         result = []
