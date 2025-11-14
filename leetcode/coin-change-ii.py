63fcb504ad7af330e49ef68170b13a1e24e35966
cxyy9256 <cxyy9256@gmail.com>
2025-11-13 20:23:47 -0500

    Add solution for coin-change-ii

---
+++ leetcode/coin-change-ii.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def change(self, amount, coins):
+        """
+        :type amount: int
+        :type coins: List[int]
+        :rtype: int
+        """
+        # dp problems - memoization
+        # O (n * amount)
+        n = len(coins)
+        dp = [0] * (amount +1)
+        dp[0] = 1
+        for i in range(n-1, -1, -1):
+            # we are taking 
+            for j in range(coins[i], amount+1):
+                dp[j] += dp[j-coins[i]]
+        return dp[amount]
